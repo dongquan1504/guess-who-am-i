b5566195cc27d0722552fbf6941c7df4a6d12f9b
dongquan1504 <dongquan1504@users.noreply.github.com>
2025-12-27 00:27:22 +0700

    feat: remove the unuse constant

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const db = getDatabase(app);
 export const firestore = getFirestore(app);
